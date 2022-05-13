1a72ad14a17fcc28d304092a1dcfb1c0c83ef8e5
Abel Akponine <abelakponine@gmail.com>
2022-05-13 22:11:35 +0100

    JSON to ASCII Graph

---
+++ stocks.js
@@ -0,0 +1,87 @@
+/** JSON to ASCII Graph: JavaScript Implementation **/
+
+let stocks = {
+    "title": "stock count",
+    "xtitle": "asset",
+    "ytitle": "count",
+    "items": [
+        {"chairs": 20},
+        {"tables": 5},
+        {"stands": 7},
+        {"lamps": 8},
+        {"cups": 10}
+    ]
+}
+
+/** This function detects and returns the highest value of items in the items list */
+generateHighestValue = (items)=>{
+    let arrValue = [];
+
+    for (x of items){
+        for (y in x){
+            let itemName = y;
+            let itemValue = x[y];
+            arrValue.push(itemValue);
+        }
+    }
+    
+    return sortArray(arrValue)[0];
+}
+
+/** This method is used for sorting items in a list from highest value to lowest value */
+sortArray = (arr)=>{
+    return arr.sort((a,b)=>{ return b-a})
+}
+
+/** This fuction builds the individual item charts, returns a string of the build chart */
+buildChart = (items)=>{
+    let hv = generateHighestValue(items); // highest value in items list
+    let i = hv;
+    let result = "";
+
+    for (i; (i <= hv && i > 0);){
+        
+        
+        for (x of items){
+            for (y in x){
+                let itemName = y;
+                let itemValue = x[y];
+                if (i <= itemValue){
+                    result += "*        ";
+                }
+                else {
+                    result +="         ";
+                }
+            }
+        }
+        result += "\r\n";
+        i = i-1;
+    }
+
+    return result.trim()+"\r\n";
+}
+
+/** This function is used for getting individual item names */
+getItemsName = (list)=>{
+    let result = "\r";
+    for (x of list){
+        for (y in x){
+            result += y+"   ";
+        }
+    }
+    return result;
+}
+
+/** This is a fuction that combines and build the overall graph, it returns a string of the build graph */
+constructGraph = (items)=>{
+    let result = `                ${stocks.title}
+                -----------
+${stocks.ytitle}
+-----
+`;
+    result += buildChart(items);
+    result += getItemsName(items);
+    return result;
+}
+
+console.log(constructGraph(stocks.items)) // display on console
